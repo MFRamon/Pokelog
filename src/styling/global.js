a0398b96518e2a6ec3dfcc8de6f3bcc2a6164d77
MFRamon <ramon.manfig@gmail.com>
2020-04-30 17:50:24 -0500

    Resetting Application Styles

--- src/styling/global.js
+++ src/styling/global.js
@@ -1,1 +1,35 @@
-// BASE FILE
+import { createGlobalStyle } from 'styled-components';
+import FoundryGridnikWoff2 from '../res/fonts/regular/Foundry-Gridnik-Regular.woff2';
+import FoundryGridnikWoff from '../res/fonts/regular/Foundry-Gridnik-Regular.woff';
+
+// eslint-disable-next-line import/prefer-default-export
+export const GlobalStyles = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Affogato Regular';
+    src: url(${FoundryGridnikWoff}) format('woff'),
+      url(${FoundryGridnikWoff2}) format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${({ theme }) => theme.main};
+    color: ${({ theme }) => theme.main};
+    font-family: Affogato Regular;
+    font-weight: 400;
+    font-size: 1.8em;
+  }  
+`;
