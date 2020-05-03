8f14aba4df7c0e0951f4f8032106b4213dd56493
MFRamon <ramon.manfig@gmail.com>
2020-05-03 10:41:14 -0500

    First Tests for Components

--- src/tests/components/atoms/Paragraph.spec.jsx
+++ src/tests/components/atoms/Paragraph.spec.jsx
@@ -1,1 +1,13 @@
-// BASE FILE
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Paragraph from '../../../components/atoms/Paragraph';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Paragraph />', () => {
+  it('renders component', () => {
+    const wrapper = shallow(<Paragraph />);
+    expect(wrapper).toBeTruthy();
+  });
+});
--- src/tests/components/common/Card.spec.jsx
+++ src/tests/components/common/Card.spec.jsx
@@ -1,1 +1,13 @@
-// BASE FILE
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Card from '../../../components/common/Card';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Card />', () => {
+  it('renders component', () => {
+    const wrapper = shallow(<Card />);
+    expect(wrapper).toBeTruthy();
+  });
+});
